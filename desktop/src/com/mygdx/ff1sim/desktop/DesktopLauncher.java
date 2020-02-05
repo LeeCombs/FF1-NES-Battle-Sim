33fe1dbcf7963da551c01d57ba62c1522e975f2f
HellaBored <leecombs_30@hotmail.com>
2020-02-05 07:43:48 -0500

    Initial commit
    Final Fantasy 1 NES Battle Simulator. Not unlike the haxeflixel project, but will include customizable heroes instead of just monsters.
    
    Java, LibGDX, Gradle I guess.

---
+++ desktop/src/com/mygdx/ff1sim/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.ff1sim.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.ff1sim.FF1Sim;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FF1Sim(), config);
+	}
+}
