1e7d272a1f51b4f4f346c6060e9918cc1f71094f
HellaBored <leecombs_30@hotmail.com>
2020-02-15 18:00:35 -0500

    Libgdx Tutorial  "Loading the assets"

--- core/src/com/fractalpixels/ff1sim/FF1Sim.java
+++ core/src/com/fractalpixels/ff1sim/FF1Sim.java
@@ -2,6 +2,8 @@ package com.fractalpixels.ff1sim;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -10,10 +12,28 @@ public class FF1Sim extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	
+	// Testing
+	private Texture dropImage, bucketImage;
+	private Sound dropSound;
+	private Music rainMusic;
+	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		
+		// load images for droplet and bucket at 64x64 each
+		dropImage = new Texture(Gdx.files.internal("images/droplet.png"));
+		bucketImage= new Texture(Gdx.files.internal("images/bucket.png"));
+		
+		// Load music and sounds
+		dropSound = Gdx.audio.newSound(Gdx.files.internal("sounds/AttackSpell.wav"));
+		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("music/BattleScene.ogg"));
+		
+		// start music
+		rainMusic.setLooping(true);;
+		rainMusic.play();
+		
 	}
 
 	@Override
--- desktop/src/com/fractalpixels/ff1sim/desktop/DesktopLauncher.java
+++ desktop/src/com/fractalpixels/ff1sim/desktop/DesktopLauncher.java
@@ -7,6 +7,12 @@ import com.fractalpixels.ff1sim.FF1Sim;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		
+		// Setup configuration
+		config.title = "FF1 NES Battle Sim";
+		config.width = 800;
+		config.height = 400;
+		
 		new LwjglApplication(new FF1Sim(), config);
 	}
 }
